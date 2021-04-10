56939b669c16019d038e6ef0884409d7cca4aee3
alector <panagiotispp@gmail.com>
2021-04-10 10:57:51 +0200

    final

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import ShoppingApp from "./components/ShoppingApp";
+
+function App() {
+	return (
+		<div className="container">
+			<Header />
+			<ShoppingApp />
+		</div>
+	);
+}
+
+export default App;
